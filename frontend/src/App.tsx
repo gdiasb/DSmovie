2d7334007709753f90449dc80f7e02aa70965516
gabriela <gdias@peq.coppe.ufrj.br>
2022-05-10 18:50:11 -0300

    :lipstick: Add Sass as CSS pre-processor
    Removed react-bootstrap library and added Sass

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Oi</h1>
+      <header>
+        <Navbar />
+      </header>
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import './navbar.style.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container-fluid nav-content">
+        <a className='navbar-brand nav-logo' href='#'>
+          DSmovie
+        </a>
+        <a className='' href=""></a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar
