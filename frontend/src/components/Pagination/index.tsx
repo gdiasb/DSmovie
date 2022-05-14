da26c69f88649e105b8d5f719ca618a4d4ef4b60
gabriela <gdias@peq.coppe.ufrj.br>
2022-05-14 16:30:52 -0300

    :lock: Security config
    Add security configuration file and update pom.xml;

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -3,14 +3,14 @@ import { FiChevronLeft, FiChevronRight} from "react-icons/fi";
 function Pagination() {
   return (
     <div
-      className="container d-flex justify-content-center mt-3"
+      className="d-flex w-50 mx-auto justify-content-center mt-3"
       style={{ backgroundColor: "red" }}
     >
       <button className="btn me-3 fs-4">
         <FiChevronLeft />
       </button>
-      <p className="my-auto">{'${1} de ${3}'}</p>
-      <button className="btn ms-3 fs-4">
+      <p className="my-auto">{"${1} de ${3}"}</p>
+      <button className="ms-3 fs-4">
         <FiChevronRight />
       </button>
     </div>
