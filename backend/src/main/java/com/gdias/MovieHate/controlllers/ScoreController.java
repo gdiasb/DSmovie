56495231fd35a9a96710640dfdcdea1139f7c3ec
Gabriela Dias <gdias@peq.coppe.ufrj.br>
2022-05-31 09:30:32 -0300

    :bug: Update to work on WSL
    JDBC URL changed to jdbc:h2:mem:testdb

--- backend/src/main/java/com/gdias/MovieHate/controlllers/ScoreController.java
+++ backend/src/main/java/com/gdias/MovieHate/controlllers/ScoreController.java
@@ -1,19 +1,25 @@
 package com.gdias.MovieHate.controlllers;
 
+import com.gdias.MovieHate.dto.MovieDTO;
+import com.gdias.MovieHate.dto.ScoreDTO;
+import com.gdias.MovieHate.services.ScoreService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gdias.MovieHate.dto.MovieDTO;
-import com.gdias.MovieHate.dto.ScoreDTO;
-
 @RestController
 @RequestMapping(value = "/scores")
 public class ScoreController {
 	
+	@Autowired
+	private ScoreService service;
+
 	@PutMapping
 	public MovieDTO saveScore(@RequestBody ScoreDTO dto) {
-		return null;
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
 	}
 }
--- backend/src/main/java/com/gdias/MovieHate/services/ScoreService.java
+++ backend/src/main/java/com/gdias/MovieHate/services/ScoreService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gdias.MovieHate.dto.ScoreDTO;
+import com.gdias.MovieHate.dto.MovieDTO;
 import com.gdias.MovieHate.entities.Movie;
 import com.gdias.MovieHate.entities.Score;
 import com.gdias.MovieHate.entities.User;
@@ -25,7 +26,7 @@ public class ScoreService {
 	private ScoreRepository scoreRepository;
 	
 	@Transactional
-	public void saveScore(ScoreDTO dto) {
+	public MovieDTO saveScore(ScoreDTO dto) {
 		
 		User user = userRepository.findByEmail(dto.getEmail());
 		if (user == null) {
@@ -54,6 +55,7 @@ public class ScoreService {
 		movie.setCount(movie.getScores().size());
 		
 		movie = movieRepository.save(movie);
-	}
 
+		return new MovieDTO(movie);
+	}
 }
