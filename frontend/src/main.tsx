80022a8bd0110de86c3a84a0f71c604eb0a38940
Gabriela Dias <gdias@peq.coppe.ufrj.br>
2022-06-04 11:49:48 -0300

    :art: Frontend files refactoring

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './main.scss';
+
+import Navbar from './components/Navbar/navbar';
+
+import Home from './pages/Home/home';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <header>
+      <Navbar />
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/list" element={<List />} />
+      <Route path="/edit">
+        <Route path=":movieId" element={<Edit />} />
+      </Route> */}
+    </Routes>
+  </BrowserRouter>
 );
