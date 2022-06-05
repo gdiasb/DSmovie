bbddcf47907487e38feda0e76e3bddd45c8d35c3
Gabriela Dias <gdias@peq.coppe.ufrj.br>
2022-06-05 16:11:59 -0300

    :heavy_plus_sign: Add axios | First Request

--- frontend/src/components/Card/card.tsx
+++ frontend/src/components/Card/card.tsx
@@ -9,16 +9,21 @@ interface Props {
 
 function Card(props:Props) {
   return (
-    <div className="col-sm-6 col-md-4 col-lg-3 border bg-info text-center mx-auto">
-      <div className="card mx-1 my-2">
+      <div className="card border-0 mx-1 my-2 h-100">
         <img src={props.image} className="card-img-top" />
         <div className="card-body">
-          <h5 className="card-title fw-bold">{props.title}</h5>
+          <h5 className="card-title fw-bold">
+            {props.title}
+          </h5>
           <p className="card-text">{props.children}</p>
-          <a href="#" className="btn btn-secondary">Rate!</a>
+        </div>
+        <div className="card-footer border-0">
+          <a href="#" className="btn btn-secondary">
+            Rate!
+          </a>
         </div>
       </div>
-    </div>)
+  );
 }
 
 export default Card;
--- frontend/src/pages/Home/home.tsx
+++ frontend/src/pages/Home/home.tsx
@@ -1,15 +1,67 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Card from "../../components/Card/card";
+import { BASE_URL } from "../../data/requests";
+
+
+interface Props {
+  id?: number;
+  image: string;
+  title: string;
+  count?: number;
+  score?: number;
+  children?: React.ReactNode;
+}
 
 function Home() {
 
-  const cardNumber: number = 5;
-  const movieTitle: string = 'Movie Title';
-  const movieCover: string =
-    'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/yDI6D5ZQh67YU4r2ms8qcSbAviZ.jpg';
-  
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?size=12`)
+      .then(response => {
+        setData(response.data.content);
+
+        setPage(response.data.totalPages);
+      })
+    }, [])
+    
+    console.log(page);
+
   return (
-    <div className="container row bg-light mx-auto mt-5">
-        {[...Array(cardNumber)].map(() => {return <Card image={movieCover} title={movieTitle}>texto</Card>})}
+    <div className="container row gy-4 mx-auto mt-5">
+      {data.map(
+        ({ count, id, title, image, score }: Props) => {
+          return (
+            <div
+              className="col-sm-6 col-md-4 col-lg-3 text-center mx-auto"
+              key={id}>
+              <Card image={image} title={title}>
+                {count}
+              </Card>
+            </div>
+          );
+        },
+      )}
+
+      <nav aria-label="Page navigation">
+        <ul className="pagination">
+          <li className="page-item">
+            <a
+              className="page-link"
+              href="#"
+              aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
