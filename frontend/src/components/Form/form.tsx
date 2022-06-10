304daa44e6d54c8ae64aa0c0f64862e95e2a4cb6
Gabriela Dias <gdias@peq.coppe.ufrj.br>
2022-06-10 11:00:35 -0300

    :construction: Create Stars Component to rate page

--- frontend/src/components/Form/form.tsx
+++ frontend/src/components/Form/form.tsx
@@ -1,25 +1,31 @@
+import Stars from "../StarsRow/stars";
+
 function Form({title}:{title: string}) {
   return (
-
-    <form className="col-lg-4 me-5">
-      <h1>{title}</h1>
-        <div className="mb-3">
-          <label
-            htmlFor="inputEmail"
-            className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="inputEmail"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" className="form-text">
+    <form className="col-lg-4 me-5 ">
+      <h1 className="mb-4">{title}</h1>
+      <div className="mb-3">
+        <label htmlFor="inputEmail" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="inputEmail"
+          aria-describedby="emailHelp"
+        />
+        {/* <div id="emailHelp" className="form-text">
             We'll never share your email with anyone else.
-          </div>
+          </div> */}
+        <Stars />
+
+        <div className="d-flex justify-content-center align-items-center mt-3">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </div>
-      </form>
+      </div>
+    </form>
   );
 }
 
